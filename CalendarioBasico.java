46c0a0763ab46c130745f3f78c92368c0664b720
nachob99 <nachoarriola99@gmail.com>
2017-11-01 19:42:06 +0100

    Nos permite introducir una fecha

---
+++ CalendarioBasico.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class CalendarioBasico here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico
+{
+    // instance variables - replace the example below with your own
+    private int dia;
+    private int mes;
+    private int ano;
+    
+
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+        dia = 1;
+        mes = 1;
+        ano = 1;
+    }
+    /**
+     * Avanzar de fecha dia a dia
+     */
+    public void avanzarFecha()
+    {
+        
+    }
+    
+    /**
+     * Fijar fecha
+     */
+    public void fijarFecha(int dia, int mes, int ano)
+    {
+        dia = dia;
+        mes = mes;
+        ano = ano;
+    }
+}
